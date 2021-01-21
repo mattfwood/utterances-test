2de470d8465081aabaf2fd9a051d88ff3b0f6e02
Matt Wood <matt@cratebind.com>
2021-01-21 12:48:05 -0600

    Move header to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
+import { BaseStyles, Button, Flex, Heading } from '@primer/components'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import '../styles/globals.css'
+import { Container } from '.';
+import Link from 'next/link';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: `Bearer 0f7a63cf0fee7a6e4aa2f04f500df3729dcd30c7`,
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <BaseStyles>
+        <Container>
+          <Flex alignItems="center" justifyContent="space-between">
+            <Link href="/">
+            <a>
+              <Heading as="h1">Message Bored</Heading>
+            </a>
+          </Link>
+        <Button as="a" href="https://github.com/mattfwood/utterances-test/issues/new" target="_blank" rel="noopener">
+          Create Post
+        </Button>
+          </Flex>
+
+          <Component {...pageProps} />
+        </Container>
+      </BaseStyles>
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
